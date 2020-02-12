7cbbf4938ed22c3412a6903b892ef5e4cbeac732
Omari-Fuller <omarif2003@gmail.com>
2020-02-12 23:14:44 +0000

    next time add everything to the list

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-let songList= ["dior", "thebox","noticeme","ballin","leanwitme"]
+let songList = ["Dior", "TheBox","NoticeMe","Ballin","Leanwitme"]
+
+let songLength = ["300", "240", "280", "180", "200"]
+
 
 songList.forEach(function(song) {
-$('.songlist').append (song  +"</br>");
+    $('#songs').append("<ul>" + song  + "</ul>");
 });
+
+songLength.forEach(function(song) {
+    $(songlength).append (song +"</br>")
+});
+
+
